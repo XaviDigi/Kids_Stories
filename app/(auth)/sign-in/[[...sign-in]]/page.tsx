0194285e88c0ed014778982752447e9aecd5ca9b
Xavier <157536486+XaviDigi@users.noreply.github.com>
2024-10-04 11:46:47 +0900

    Update page.tsx

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,16 +1,23 @@
-import { SignIn } from '@clerk/nextjs'
-import Image from 'next/image'
+import { SignIn } from '@clerk/nextjs';
+import Image from 'next/image';
 
 export default function Page() {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2'>
-        <div>
-          <Image src={'/login.png'} alt='login' 
-          width={700} height={1000} className='w-full'/>
-        </div>
-        <div className='flex justify-center items-center h-screen order-first md:order-last'>
-          <SignIn/>
-        </div>
+      <div className='flex justify-center items-center'>
+        <Image
+          src={'/login.png'}
+          alt='login'
+          width={700}
+          height={1000}
+          className='w-full h-auto max-w-md md:max-w-lg'
+        />
+      </div>
+      <div className='flex flex-col justify-center items-center h-screen order-first md:order-last bg-neutral-light p-5'>
+      <h2 className="text-2xl font-bold mb-4">Welcome!</h2>
+      <p className="text-lg mb-6">Sign in with Google to continue.</p>
+        <SignIn />
+      </div>
     </div>
-  )
+  );
 }
