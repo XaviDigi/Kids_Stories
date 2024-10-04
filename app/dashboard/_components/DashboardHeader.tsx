3916df8788e8f4f8449cc07f24db75fa6c5134b1
Xavier <157536486+XaviDigi@users.noreply.github.com>
2024-10-04 11:30:25 +0900

    Update DashboardHeader.tsx

--- app/dashboard/_components/DashboardHeader.tsx
+++ app/dashboard/_components/DashboardHeader.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { UserDetailContext } from '@/app/_context/UserDetailConext';
 import { Button } from '@nextui-org/button';
 import Image from 'next/image';
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import React, { useContext } from 'react';
 
 function DashboardHeader() {
-  const { userDetail, setUserDetail } = useContext(UserDetailContext);
+  const { userDetail } = useContext(UserDetailContext);
 
   return (
     <header className="p-5 bg-gradient-to-r from-blue-500 to-purple-600 text-white shadow-lg">
@@ -15,18 +15,18 @@ function DashboardHeader() {
           My Stories
         </h2>
 
-        <div className="flex gap-4 items-center">
-          <Image src={'/coin.png'} alt='coin' width={25} height={25} />
-
-          <span className="text-xl md:text-2xl font-medium">
-            {userDetail?.credit} Credit Left
-          </span>
+        <div className="flex flex-col gap-2 items-center">
+          <div className="flex items-center">
+            <Image src={'/coin.png'} alt='coin' width={25} height={25} />
+            <span className="text-xl md:text-2xl font-medium">
+              {userDetail?.credit} Credit(s) Left
+            </span>
+          </div>
 
           <Link href="/buy-credits">
             <Button
               className="bg-blue-400 hover:bg-blue-600 px-3 py-1 rounded-full shadow-md transition-all duration-200 ease-in-out"
               color="secondary"
-           
             >
               Buy More Credits
             </Button>
